5ad55343878d612ce5b30d27cb9eb578d6e3f004
Antonio Olaya <138058637+AntonioOlay@users.noreply.github.com>
2024-04-07 12:07:53 -0600

    Add files via upload

---
+++ jupyter_server_config.py
@@ -0,0 +1,11 @@
+c.ServerProxy.servers = {
+           'noVNC': {
+                   'command': ['/home/amy/.vnc/xstartup'],
+                    'timeout': 30,
+                    'port': 6080,
+                    'absolute_url': False,
+                    'launcher_entry': {
+                        'title': 'noVNC'
+               }
+          }
+    }
---
+++ noVNC.py
@@ -0,0 +1,5 @@
+def setup_noVNC():
+    return {
+            "command": ["/home/amyserver/.vnc/xstartup"]
+            }
+
