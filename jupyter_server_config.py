4d9d502f3e1864803804fb6cdb1363027907a1a4
Antonio Olaya <138058637+AntonioOlay@users.noreply.github.com>
2024-04-16 11:59:06 -0600

    Add files via upload

--- jupyter_server_config.py
+++ jupyter_server_config.py
@@ -1,13 +1,16 @@
 import os
+import os.path
 
 uid=os.getuid()
 
 WEBSOCKET=8900+1000+uid
 print(WEBSOCKET)
 
+xfec_path = os.path.abspath("xfec_desk3.sh")
+
 c.ServerProxy.servers = {
            'noVNC': {
-                   'command': ['/home/amyserver/xfec_desk3.sh'],
+                   'command': [xfec_path],
                     'timeout': 30,
                     'port': WEBSOCKET,
                     'absolute_url': False,
